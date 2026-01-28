712e6a9cf493298641c13ff8a831d0b61b297594
kskr1007 <krishop@outlook.com>
2026-01-28 16:53:52 -0500

    Added TextWatching on username

--- app/src/main/java/com/example/week3intent/MainActivity.kt
+++ app/src/main/java/com/example/week3intent/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.week3intent
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -28,12 +31,56 @@ class MainActivity : AppCompatActivity() {
         myPassword = findViewById(R.id.password)
         myButton = findViewById(R.id.button)
 
+        // add listener on this field
+        myUsername.addTextChangedListener(myTextWatcher)
+
         myButton.setOnClickListener {
-            val mySecondScreenIntent = Intent(this, MainActivity2::class.java)
-            mySecondScreenIntent.putExtra("location", "my city")
-            startActivity(mySecondScreenIntent)
+            // get username
+            val username = myUsername.text.toString()
+            // check if blank
+            if(username.isBlank()){
+                // error message
+                Toast.makeText(applicationContext, "Username needed!", Toast.LENGTH_LONG).show()
+
+            }
+            // move to next activity
+            else {
+                val mySecondScreenIntent = Intent(this, MainActivity2::class.java)
+                mySecondScreenIntent.putExtra("location", "my city")
+                startActivity(mySecondScreenIntent)
+            }
         }
     }
+
+    private val myTextWatcher: TextWatcher = object: TextWatcher{
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+        override fun beforeTextChanged(
+            s: CharSequence?,
+            start: Int,
+            count: Int,
+            after: Int
+        ) {
+
+        }
+
+        override fun onTextChanged(
+            s: CharSequence?,
+            start: Int,
+            before: Int,
+            count: Int
+        ) {
+            // getting the username while it is being typed
+            val inputtedUsername = myUsername.text.toString()
+            // boolean to enable login once username has a value
+            val enableButton = inputtedUsername.isNotBlank()
+            // enable button if true
+            myButton.isEnabled = enableButton
+        }
+    }
+
     override fun onPause(){
         super.onPause()
         lifecycleLog("in on pause")
